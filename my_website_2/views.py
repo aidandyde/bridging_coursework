ed441c8d9fee8f6ba68ccbf19461a7f3be67d765
Aidan Dyde <adyde@DESKTOP-5A990CS.lan>
2020-06-07 11:03:15 +0100

    modified templates

--- my_website_2/views.py
+++ my_website_2/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
+from	django.shortcuts	import	render
+from	django.utils	import	timezone
+from	.models	import	Post
 def	post_list(request):
-				return	render(request,	'my_website_2/post_list.html',	{})
+				posts	=	Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+				return	render(request,	'my_website_2/post_list.html',	{'posts':posts})
